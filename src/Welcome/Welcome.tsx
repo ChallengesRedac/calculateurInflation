302ecfc28c61d19a810336bdb954b9da6afdd211
rm951 <49212242+rm951@users.noreply.github.com>
2023-06-13 15:27:44 +0200

    Update Welcome.tsx

--- src/Welcome/Welcome.tsx
+++ src/Welcome/Welcome.tsx
@@ -1,12 +1,36 @@
 import { Button, Stack, Text } from '@mantine/core';
+import { createStyles, getStylesRef, rem } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    maxWidth: rem(400),
+    width: '100%',
+    height: rem(180),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    borderRadius: theme.radius.sm,
+
+    // Dynamic media queries, define breakpoints in theme, use anywhere
+    [theme.fn.smallerThan('sm')]: {
+      // Child reference in nested selectors via ref
+      [`& .${getStylesRef('child')}`]: {
+        fontSize: theme.fontSizes.xs,
+      },
+    },
+  },
+
+  
+}));
 
 export function Welcome() {
+  const { classes } = useStyles();
+
   return (
-    <Stack align="center" mt={50}>
-      <Text size="xl" weight={500}>
-        Welcome to Mantine!
-      </Text>
-      <Button>Cliquez sur le bouton</Button>
-    </Stack>
+    <div className={classes.wrapper}>
+      Coucou
+    </div>
   );
 }
